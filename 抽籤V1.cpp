ea07c8a9002bee6d83c4ea9a0a004d2e3aafe5aa
SnoopyKnight <thedarkknight01@icloud.com>
2017-08-12 20:33:43 +0800

    Add files via upload

---
+++ 抽籤V1.cpp
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+int n;
+int player(int f, int *p)
+{
+	*p = n;
+	f == rand() % n ;
+	return f++;
+}
+
+void game(char a[])
+{
+	int game,i;
+	a[0] = {"真心話大冒險"}
+	a[1] = {"表演一項特技"}
+	a[2] = {""} //.....
+	i = 0
+
+		game = a[i]
+}
+
+int main()
+{
+	printf("設定成員人數:");
+	scanf("%d", n);
+
+
+	while (true)
+	{
+		printf("代號:%d",& player(f));
+		scanf("%s", game(a));
+
+	}
+	
+}
